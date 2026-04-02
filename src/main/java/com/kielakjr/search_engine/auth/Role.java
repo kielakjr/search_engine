4649ab8f62e1b3f703770960646e22b21d3a0fdc
mkielak <kielakowsky17@gmail.com>
2026-04-02 12:00:47 +0200

    Added base for user with repo and model

---
+++ src/main/java/com/kielakjr/search_engine/auth/Role.java
@@ -0,0 +1,6 @@
+package com.kielakjr.search_engine.auth;
+
+public enum Role {
+  USER,
+  ADMIN
+}
---
+++ src/main/java/com/kielakjr/search_engine/auth/User.java
@@ -0,0 +1,36 @@
+package com.kielakjr.search_engine.auth;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(unique = true, nullable = false)
+  private String email;
+  @Column(nullable = false)
+  private String password;
+
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+  @CreationTimestamp
+  private LocalDate createdAt;
+}
